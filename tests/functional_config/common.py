7228661bedb2190eb6affe8995b20f4411620a48
Crul <rauljavier.vila@gmail.com>
2018-07-06 18:17:42 +0200

    v1.3.1 - Functional testing of ignore features

--- tests/functional_config/common.py
+++ tests/functional_config/common.py
@@ -5,6 +5,13 @@ ExpectedResults = \
 
 ExpectedResults.__new__.__defaults__ = (0, 0, 0)
 
-FuncTestConfig = namedtuple("TestConfig", ["id", "setup", "teardown", "expected_results"])
+FuncTestConfig = namedtuple("TestConfig", ["id",
+                                           "setup",
+                                           "teardown",
+                                           "expected_results",
+                                           "ignore_untracked_files",
+                                           "ignore_files_regex"])
+
+FuncTestConfig.__new__.__defaults__ = (None, None)
 
 test_configs = []
